a065c818d6d5514d218483fd45ac7ada42fd074c
Tuong <huuluong2027@gmail.com>
2021-07-11 14:07:26 +0700

    11/7

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -79,6 +79,10 @@ namespace LeHuuTuong_BigSchool001.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
