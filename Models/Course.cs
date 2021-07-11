8315315a99cc37f932bbb8d2b3aab802757fc201
Tuong <huuluong2027@gmail.com>
2021-07-11 22:37:26 +0700

    10:37 11/7

--- Models/Course.cs
+++ Models/Course.cs
@@ -9,6 +9,7 @@ namespace LeHuuTuong_BigSchool001.Models
     public class Course
     {
         public int Id { get; set; }
+        public bool IsCanceled { get; set; }
         public ApplicationUser Lecture { get; set; }
         [Required]
         public string LectureId { get; set; }
